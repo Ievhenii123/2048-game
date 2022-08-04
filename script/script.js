127ef4842175f33fca2672f094bdc5ae8d2fa27e
Ievhenii <geny1990@ukr.net>
2022-08-04 22:28:55 +0300

    Making game logic...

--- script/script.js
+++ script/script.js
@@ -1,6 +1,53 @@
 var canvas = document.getElementById("2048-field");
 var ctx = canvas.getContext("2d");
 
+let logicArrMatrix = [
+   [ [ {a: 2} ], [ {a: 2} ], [ {a: 2} ], [ {a: 4} ] ],
+   [ [], [], [], [] ],
+   [ [], [], [], [] ],
+   [ [], [], [], [] ]
+];
+
+let calcSameElems = {
+    fromRight: function(logicArrMatrix) {
+        moveArrElems.right(logicArrMatrix);
+        for (let i = 3; i >= 1; i--) {
+            let curElem = logicArrMatrix[0][i][0],
+                prevElem = logicArrMatrix[0][i-1][0];
+            if (typeof curElem !== 'undefined' && typeof prevElem !== 'undefined' && curElem.a == prevElem.a) {
+                logicArrMatrix[0][i][0].a = curElem.a + prevElem.a;
+                logicArrMatrix[0].splice(i-1, 1, []);
+                moveArrElems.right(logicArrMatrix);
+            }
+        }
+        return logicArrMatrix;
+    }
+}
+
+let moveArrElems = {
+    right: function(logicArrMatrix) {
+        for (let j = 0; j <= 3; j++) {
+            for (let i = 3; i >= 1; i--) {
+                if (typeof logicArrMatrix[j][i][0] === 'undefined') {
+                    logicArrMatrix[j].splice(i, 1);
+                }
+            }
+            if (logicArrMatrix[j].length < 4) {
+                let toFill = 4 - logicArrMatrix[j].length;
+                for (let i = 1; i <= toFill; i++) {
+                    logicArrMatrix[j].unshift([]);
+                }
+            }
+        }
+        return logicArrMatrix;
+    },
+    left: function(logicArrMatrix) {
+
+    }
+}
+calcSameElems.fromRight(logicArrMatrix);
+console.log(logicArrMatrix);
+
 // Рисуем квадрат, и тестируем его перемещение по полю по нажатию клавиш клавиатуры (стрелки или WASD)
 let brick = {
     x: 0,
@@ -16,10 +63,12 @@ let brick = {
 
 let grid = {
     marginBetweenCells: 12,
-    gridCellStartPosX: 12,
-    gridCellStartPosY: 12,
-    gridCellHeight: 97.5,
-    gridCellWidth: 97.5,
+    cellStartPosX: 12,
+    cellStartPosY: 12,
+    cellHeight: 97.5,
+    cellWidth: 97.5,
+    // Вычисления координат сетки игрового поля 
+    // (в будущем подумать над оптимизацией вычислений, так как выполяняются повторные вычисления уже вычисленных значений)
     calcGridCoords: function() {
         let coordsArr = [],
             coordX = 0, 
@@ -27,24 +76,25 @@ let grid = {
         for (let row = 1; row <= 4; row++) {
             for (let col = 1; col <= 4; col++) {
                 if (col <= 1) {
-                    coordX = this.gridCellStartPosX;
+                    coordX = this.cellStartPosX;
                     if (row <= 1) {
-                        coordY = this.gridCellStartPosY;
+                        coordY = this.cellStartPosY;
                     } else {
-                        coordY = this.gridCellHeight * (row-1) + this.marginBetweenCells * row
+                        coordY = this.cellHeight * (row-1) + this.marginBetweenCells * row
                     }
                 } else {
-                    coordX = this.gridCellWidth * (col-1) + this.marginBetweenCells * col
+                    coordX = this.cellWidth * (col-1) + this.marginBetweenCells * col
                 }
                 coordsArr.push( {x: coordX, y: coordY} );
             }
         }
         return coordsArr;
     },
+    // Рисуем сетку по полученным координатам
     draw: function(coordsArr) {
         for (coord of coordsArr) {
             ctx.beginPath();
-            ctx.rect(coord.x, coord.y, this.gridCellWidth, this.gridCellHeight);
+            ctx.rect(coord.x, coord.y, this.cellWidth, this.cellHeight);
             ctx.fillStyle = "rgba(238,228,218,.35)";
             ctx.fill();
             ctx.closePath();
